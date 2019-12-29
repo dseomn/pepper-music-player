792109c70ffd1ded5d279a74d1297af7859a08cb
David Mandelberg <dseomn@google.com>
2019-12-28 20:37:26 -0500

    Add module to work with the library database.

---
+++ pepper_music_player/library/database.py
@@ -0,0 +1,82 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Database for a library."""
+
+import sqlite3
+import threading
+
+_SCHEMA_DROP = (
+    'DROP TABLE IF EXISTS File',
+    'DROP TABLE IF EXISTS AudioFileTag',
+)
+
+_SCHEMA_CREATE = (
+    # Files in the library.
+    #
+    # Columns:
+    #   dirname: Absolute name of the directory containing the file.
+    #   filename: Name of the file, relative to dirname.
+    """
+    CREATE TABLE File (
+      dirname TEXT NOT NULL,
+      filename TEXT NOT NULL,
+      PRIMARY KEY (dirname, filename)
+    )
+    """,
+
+    # Tags for audio files in the library.
+    #
+    # Columns:
+    #   file_id: Which file has the tag.
+    #   tag_name: Name of the tag, e.g., 'artist'. Each value may appear
+    #     multiple times for the same file_id.
+    #   tag_value: A single value for the tag.
+    """
+    CREATE TABLE AudioFileTag (
+      file_id INTEGER NOT NULL REFERENCES File (rowid) ON DELETE CASCADE,
+      tag_name TEXT NOT NULL,
+      tag_value TEXT NOT NULL
+    )
+    """,
+    'CREATE INDEX AudioFileTag_FileIndex ON AudioFileTag (file_id)',
+    'CREATE INDEX AudioFileTag_TagIndex ON AudioFileTag (tag_name, tag_value)',
+)
+
+
+class Database:
+  """Database for a library."""
+
+  def __init__(self, sqlite3_path: str) -> None:
+    """Initializer.
+
+    Args:
+      sqlite3_path: Path to a sqlite3 database.
+    """
+    self._sqlite3_path = sqlite3_path
+    self._local = threading.local()
+
+  @property
+  def _connection(self) -> sqlite3.Connection:
+    # https://docs.python.org/3.8/library/sqlite3.html#multithreading says that
+    # sqlite3 connections shouldn't be shared between threads.
+    if not hasattr(self._local, 'connection'):
+      self._local.connection = sqlite3.connect(
+          self._sqlite3_path, isolation_level=None)
+    return self._local.connection
+
+  def reset(self) -> None:
+    """(Re)sets the database to its initial, empty state."""
+    with self._connection:
+      for statement in _SCHEMA_DROP + _SCHEMA_CREATE:
+        self._connection.execute(statement)
---
+++ pepper_music_player/library/database_test.py
@@ -0,0 +1,55 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for pepper_music_player.library.database."""
+
+import os
+import sqlite3
+import tempfile
+import unittest
+
+from pepper_music_player.library import database
+
+
+class DatabaseTest(unittest.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    tempdir = tempfile.TemporaryDirectory()
+    self.addCleanup(tempdir.cleanup)
+    sqlite3_path = os.path.join(tempdir.name, 'database.sqlite3')
+    self._database = database.Database(sqlite3_path)
+    self._database.reset()
+    self._connection = sqlite3.connect(sqlite3_path, isolation_level=None)
+
+  def test_reset_deletes_data(self):
+    with self._connection:
+      file_id = self._connection.execute(
+          'INSERT INTO File (dirname, filename) VALUES ("a", "b")').lastrowid
+      self._connection.execute(
+          """
+          INSERT INTO AudioFileTag (file_id, tag_name, tag_value)
+          VALUES (?, "c", "d")
+          """,
+          (file_id,),
+      )
+    self._database.reset()
+    with self._connection:
+      self.assertFalse(
+          self._connection.execute('SELECT * FROM File').fetchall())
+      self.assertFalse(
+          self._connection.execute('SELECT * FROM AudioFileTag').fetchall())
+
+
+if __name__ == '__main__':
+  unittest.main()
