6f904c170f0899428249e0ebe15456fe12cf4476
David Mandelberg <dseomn@google.com>
2020-02-01 21:26:27 -0500

    Add helper to construct the window.

--- pepper_music_player/ui/application_test.py
+++ pepper_music_player/ui/application_test.py
@@ -60,6 +60,10 @@ class ApplicationTest(unittest.TestCase):
             database_dir=tempdir.name,
         )
 
+    def _window(self):
+        return application.window(self._library_db, self._player,
+                                  self._playlist)
+
     def test_install_css_does_not_raise_exceptions(self):
         # The function is called twice since it has a separate code path if the
         # css is already installed.
@@ -67,13 +71,10 @@ class ApplicationTest(unittest.TestCase):
         application.install_css()
 
     def test_blank(self):
-        _register_screenshot(
-            'test_blank',
-            application.window(self._library_db, self._player, self._playlist))
+        _register_screenshot('test_blank', self._window())
 
     def test_exit_stops_main_loop(self):
-        window = application.window(self._library_db, self._player,
-                                    self._playlist)
+        window = self._window()
         window.show_all()
         GLib.idle_add(window.destroy)
         # This just tests that Gtk.main doesn't run forever.
