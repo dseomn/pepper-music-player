e1d4b1da2cf3f56e7dfc5f4b3f0f5cf2c20f6154
David Mandelberg <dseomn@google.com>
2020-02-29 23:28:17 -0500

    Make it easier to scan the library from the command line.
    This is a simple hack, not a permanent solution.

--- pepper_music_player/main.py
+++ pepper_music_player/main.py
@@ -13,11 +13,14 @@
 # limitations under the License.
 """Main application."""
 
+import os
+
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 from pepper_music_player.library import database
+from pepper_music_player.library import scan
 from pepper_music_player.player import player
 from pepper_music_player.player import playlist
 from pepper_music_player import pubsub
@@ -28,6 +31,11 @@ def main() -> None:
     # TODO(dseomn): Switch to the real default database_dir, once there is one.
     database_dir = '.'
     library_db = database.Database(database_dir=database_dir)
+    # TODO(dseomn): Make scanning controllable by the UI instead of doing it
+    # here.
+    library_scan_dir = os.getenv('PEPPER_SCAN')
+    if library_scan_dir:
+        library_db.insert_files(scan.scan(library_scan_dir))
     pubsub_bus = pubsub.PubSub()
     player_ = player.Player(pubsub_bus=pubsub_bus)
     playlist_ = playlist.Playlist(
