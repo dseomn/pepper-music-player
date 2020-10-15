fda9dae0a01607efc919917eaf942c0a574cc8a5
David Mandelberg <dseomn@google.com>
2020-10-14 22:00:56 -0400

    Skip test on macOS because of gstreamer bug.

--- pepper_music_player/player/player_test.py
+++ pepper_music_player/player/player_test.py
@@ -16,6 +16,7 @@
 import datetime
 import operator
 import os
+import sys
 import tempfile
 import time
 import unittest
@@ -353,6 +354,12 @@ class PlayerTest(unittest.TestCase):
         self._player.previous()
         self.assertEqual(_AUDIO_ZEROES, self._all_audio()[-len(_AUDIO_ZEROES):])
 
+    # TODO(https://gitlab.freedesktop.org/gstreamer/gstreamer/-/issues/616):
+    # Don't skip this test.
+    @unittest.skipIf(
+        sys.platform.startswith('darwin'),
+        'https://gitlab.freedesktop.org/gstreamer/gstreamer/-/issues/616',
+    )
     def test_logs_and_stops_on_error(self):
         tempdir = tempfile.TemporaryDirectory()
         self.addCleanup(tempdir.cleanup)
